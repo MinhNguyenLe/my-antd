2bf56e09c500b71ee7d8c909b09d26aabdc53604
MinhLeeBtasekee <leminh.nguyen@btaskee.com>
2023-04-03 10:26:34 +0700

    feat: add antd base component

---
+++ src/base/antd/controllers/ControllerUpload/index.tsx
@@ -0,0 +1,21 @@
+import Form from 'antd/lib/form';
+import Upload, { UploadProps } from 'antd/lib/upload';
+import { FormItemBaseProps } from 'component-base/antd/FormBase';
+
+export interface ControllerUploadProps extends UploadProps {
+  formItemProps: FormItemBaseProps;
+}
+
+function ControllerUpload({
+  children,
+  formItemProps,
+  ...props
+}: ControllerUploadProps) {
+  return (
+    <Form.Item {...formItemProps}>
+      <Upload {...props}>{children}</Upload>
+    </Form.Item>
+  );
+}
+
+export default ControllerUpload;
